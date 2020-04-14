0a283eb9a991ceef249aa129451c84e5f165f5f4
Oleg Eterevsky <oleg@eterevsky.com>
2020-04-14 22:46:58 +0200

    Refactor the camera code and make it customizable.

--- src/camera.rs
+++ src/camera.rs
@@ -1,35 +1,79 @@
-use cgmath::{InnerSpace, Point3};
+use cgmath::{dot, InnerSpace, Matrix3, Point3, Vector3};
 use std::time;
 
 use crate::scene::Scene;
 
 pub struct Camera {
+    origin: Point3<f32>,
+    direction: Vector3<f32>,
+    up: Vector3<f32>,
+    horizontal_fov: f32,
     w: u32,
     h: u32,
-    scale: f32,
-    origin: Point3<f32>,
 }
 
 impl Camera {
-    pub fn new(w: u32, h: u32, origin: Point3<f32>) -> Self {
+    pub fn new() -> Self {
         Camera {
-            w,
-            h,
-            origin,
-            scale: 2. / (h as f32)
+            origin: Point3::new(0., 0., 1.),
+            direction: Vector3::new(0., 0., -1.),
+            up: Vector3::new(0., 1., 0.),
+            horizontal_fov: std::f32::consts::FRAC_PI_2,
+            w: 1280,
+            h: 720,
         }
     }
 
+    pub fn set_origin(mut self, origin: Point3<f32>) -> Self {
+        self.origin = origin;
+        self
+    }
+
+    pub fn set_direction(mut self, direction: Vector3<f32>) -> Self {
+        self.direction = direction.normalize();
+        self
+    }
+
+    pub fn set_up(mut self, up: Vector3<f32>) -> Self {
+        self.up = up.normalize();
+        self
+    }
+
+    pub fn set_fov(mut self, fov: f32) -> Self {
+        self.horizontal_fov = fov;
+        self
+    }
+
+    pub fn set_dimensions(mut self, w: u32, h: u32) -> Self {
+        self.w = w;
+        self.h = h;
+        self
+    }
+
     pub fn render(&self, scene: &Scene) -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>> {
+        let project_up = dot(self.direction, self.up);
+        let up = (self.up - project_up * self.direction).normalize();
+        let left = -self.direction.cross(up);
+
+        let hfov_tan = (self.horizontal_fov / 2.).tan();
+        let vfov_tan = (self.h as f32) / (self.w as f32) * hfov_tan;
+
+        let frame_left = self.direction + left * hfov_tan;
+        let frame_upleft = frame_left + up * vfov_tan;
+
+        let ray_mat = Matrix3::from_cols(
+            -left * hfov_tan * 2. / (self.w as f32),
+            -up * vfov_tan * 2. / (self.h as f32),
+            frame_upleft
+        );
+
         let mut image = image::ImageBuffer::new(self.w, self.h);
         let start = time::Instant::now();
         let mut rays: u64 = 0;
 
         for (x, y, pixel) in image.enumerate_pixels_mut() {
             rays += 1;
-            let x = (x as f32) * self.scale - 1.;
-            let y = -(y as f32) * self.scale + 1.;
-            let dir = Point3::new(x, y, 0.) - self.origin;
+            let dir = ray_mat * Vector3::new(x as f32, y as f32, 1.);
             let dir = dir.normalize();
             *pixel = scene.ray_color(self.origin, dir);
         }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,23 @@ use raytracer::*;
 
 fn main() {
     let mut scene = Scene::new();
-    scene.add_plane(Plane::new(Point3::new(0., -1., 0.), Vector3::new(0., 1., 0.)), Material::new(0.8, 0.8, 0.8));
+    scene.add_plane(
+        Plane::new(Point3::new(0., -1., 0.), Vector3::new(0., 1., 0.)),
+        Material::new(0.8, 0.8, 0.8),
+    );
 
-    scene.add_sphere(Sphere::new(Point3::new(0.0, 0.0, -3.), 1.), Material::new(0.75, 0.25, 0.25));
-    scene.add_sphere(Sphere::new(Point3::new(1.0, 3.0, -10.), 2.), Material::new(0.25, 0.65, 0.25));
-    scene.add_sphere(Sphere::new(Point3::new(0.65, 0.65, -2.3), 0.1), Material::new(0.6, 0.4, 0.2));
+    scene.add_sphere(
+        Sphere::new(Point3::new(0.0, 0.0, -3.), 1.),
+        Material::new(0.75, 0.25, 0.25),
+    );
+    scene.add_sphere(
+        Sphere::new(Point3::new(1.0, 3.0, -10.), 2.),
+        Material::new(0.25, 0.65, 0.25),
+    );
+    scene.add_sphere(
+        Sphere::new(Point3::new(0.65, 0.65, -2.3), 0.1),
+        Material::new(0.6, 0.4, 0.2),
+    );
 
     scene.add_point_light(Point3::new(0., 0.1, 3.5), 3.);
     scene.add_sphere_light(Point3::new(2., 1., 0.), 0.5, 3.);
@@ -16,7 +28,7 @@ fn main() {
     scene.add_sphere_light(Point3::new(0., 10., -5.), 1.0, 30.);
     // scene.add_sphere_light(Point3::new(-0.65, 0.65, -2.3), 0.1, 0.1);
 
-    let camera = Camera::new(1280, 720, Point3::new(0., 0., 3.));
+    let camera = Camera::new();
     let image = camera.render(&scene);
 
     image.save("image.png").unwrap();
